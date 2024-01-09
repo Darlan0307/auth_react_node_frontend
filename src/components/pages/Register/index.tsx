d79a7e14ac6e1bb69808bfce22cfaa7d85dd752d
Darlan0307 <darlanchagas2020@gmail.com>
2024-01-09 20:52:54 -0300

    :art: novos componentes de layout para as paginas do formulario

--- src/components/pages/Register/index.tsx
+++ src/components/pages/Register/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import LayoutForms from '../../LayoutForms'
 import './styles.scss'
 
@@ -5,8 +6,39 @@ const Register = () => {
   return (
     <LayoutForms>
       <main className='page-register'>
-        <h1>adasdas</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, est.</p>
+        <h1 className='title'>Sign Up</h1>
+        <p className='description'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, mollitia?</p>
+        <form className='form'>
+          <div className='wrap-input'>
+            <input 
+            type="email" 
+            placeholder='Digite seu email...'
+            className='input'
+            />
+            <span>icon</span>
+          </div>
+          <div className='wrap-input'>
+            <input 
+            type="password" 
+            placeholder='Crie uma senha...'
+            className='input'
+            />
+            <span>icon</span>
+          </div>
+          <div className='wrap-input'>
+            <input 
+            type="password" 
+            placeholder='Confirme a senha...'
+            className='input'
+            />
+            <span>icon</span>
+          </div>
+          <button type='submit'>Register</button>
+        </form>
+        <p className='sugestao'>OU</p>
+        <p className='content-link'>
+          Ja tem uma conta? <Link to="/login" className='link'>Faça login</Link>
+        </p>
       </main>
     </LayoutForms>
   )
