031128be860b390f86410216c4169e8150ab9251
Darlan0307 <darlanchagas2020@gmail.com>
2024-01-10 18:08:22 -0300

    :art: estilo da pagina de login finalizado

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,7 +1,43 @@
+import { Link } from 'react-router-dom'
+import LayoutForms from '../../LayoutForms'
+import './styles.scss'
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <LayoutForms>
+      <main className='page-login'>
+        <div className='apresentation'>
+          <h1 className='title'>Sign In</h1>
+          <p className='description'>Faça login para ter acesso aos nossos serviços e recursos.</p>
+        </div>
+        <form className='form'>
+          <div className='wrap-input'>
+            <input 
+            type="email" 
+            placeholder='Digite o seu email...'
+            className='input'
+            />
+            <span><MdEmail/></span>
+          </div>
+          <div className='wrap-input'>
+            <input 
+            type="password" 
+            placeholder='Digite a sua senha...'
+            className='input'
+            />
+            <span><RiLockPasswordFill/></span>
+          </div>
+          <button type='submit' className='button'>Login</button>
+        </form>
+        <p className='sugestao'>OU</p>
+        <p className='content-link'>
+          Não tem uma conta? <Link to="/" className='link'>Crie agora!</Link>
+        </p>
+      </main>
+    </LayoutForms>
   )
 }
 
